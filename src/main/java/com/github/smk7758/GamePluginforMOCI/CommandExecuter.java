b23bfb841b979f0d18447b69d4385fb4ff24f1ae
smk7758 <maxkovko@yahoo.co.jp>
2018-03-07 00:12:37 +0900

    ver: 0.0.2
    add command for set active or inactive.

--- src/main/java/com/github/smk7758/GamePluginforMOCI/CommandExecuter.java
+++ src/main/java/com/github/smk7758/GamePluginforMOCI/CommandExecuter.java
@@ -13,6 +13,16 @@ public class CommandExecuter implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("GamePluginforMOCI")) {
+			if (args.length < 1) {
+				SendLog.send("This is the plugin for MOCI.", sender);
+				SendLog.send("args: active", sender);
+			} else if (args[0].equalsIgnoreCase("active")) {
+				Main.active = !Main.active;
+				SendLog.send("Active status: " + Main.active, sender);
+			}
+			return true;
+		}
 		return false;
 	}
 }
--- src/main/java/com/github/smk7758/GamePluginforMOCI/Main.java
+++ src/main/java/com/github/smk7758/GamePluginforMOCI/Main.java
@@ -5,18 +5,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 	public static final String plugin_name = "GamePluginforMOCI";
 	public static boolean debug_mode = false;
-	// private CommandExecuter command_executer = new CommandExecuter(this);
+	public static boolean active = true;
+	private CommandExecuter command_executer = new CommandExecuter(this);
 	private GameListener game_listner = new GameListener(this);
 
 	@Override
 	public void onEnable() {
 		if (!Main.plugin_name.equals(getDescription().getName())) getPluginLoader().disablePlugin(this);
 		getServer().getPluginManager().registerEvents(game_listner, this);
-		// getCommand(plugin_name).setExecutor(command_executer);
+		getCommand(plugin_name).setExecutor(command_executer);
+		saveDefaultConfig();
+		active = getConfig().getBoolean("active");
+		SendLog.send("Active status: " + Main.active);
 	}
 
 	@Override
 	public void onDisable() {
+		getConfig().set("active", Main.active);
 	}
 
 	// public CommandExecuter getCommandExecuter() {
